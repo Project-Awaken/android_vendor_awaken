b8f1725fe8965abb0d06894539229954ba54f2d6
Akito Mizukito <peak@echln.net>
2019-08-09 22:10:41 +0900

    vendor: tools: Make the generator suit the current OTA system needs
    Signed-off-by: Akito Mizukito <peak@echln.net>
    
    -----
    
    vendor: tools: Fixed JSON file_size parsing
    
    Change-Id: I2829a42debb5bdcb18fd98434ab5d355a2c3e31f
    Co-authored-by: あきと ミズキト <peak@echln.net>

--- tools/generate_json_build_info.sh
+++ tools/generate_json_build_info.sh
@@ -4,10 +4,10 @@ then
   file_path=$1
   file_name=$(basename "$file_path")
   if [ -f $file_path ]; then
-    file_size=$(stat --printf="%s" "$file_path")
+    file_size=$(stat -c%s $file_path)
     md5=$(cat "$file_path.md5sum" | cut -d' ' -f1)
     datetime=$(grep ro\.build\.date\.utc $OUT/system/build.prop | cut -d= -f2);
     id=$(sha256sum $file_path | awk '{ print $1 }');
-    echo "{\n   \"file_name\": \"$file_name\",\n   \"file_size\": $file_size,\n   \"datetime\": $datetime,\n   \"id\": \"$id\",\n   \"md5\": \"$md5\"\n}" > $file_path.json
+    echo "{\n   \"filename\": \"$file_name\",\n   \"size\": $file_size,\n   \"datetime\": $datetime,\n   \"id\": \"$id\",\n   \"filehash\": \"$md5\"\n}" > $file_path.json
   fi
 fi
