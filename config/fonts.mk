f5208c2c3ed2d633bebb420a147ca9626e8d942c
Anirudh Gupta <anirudhgupta109@gmail.com>
2019-09-29 17:46:51 +0530

    vendor: fonts: Refactor fonts.mk to include all fonts
    Its way too tedious to type out each fonts' path to copy especially since 10 introduced so many styles
    
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Change-Id: I1c84ca37fea03ea7c832a003512701d0c48d10f8
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- config/fonts.mk
+++ config/fonts.mk
@@ -1,9 +1,6 @@
 # Fonts
+LOCAL_PATH := vendor/awaken/fonts
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/ttf,$(TARGET_COPY_OUT_PRODUCT)/fonts)
+
 PRODUCT_COPY_FILES += \
-    vendor/awaken/fonts/GoogleSans-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Regular.ttf \
-    vendor/awaken/fonts/GoogleSans-Medium.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Medium.ttf \
-    vendor/awaken/fonts/GoogleSans-MediumItalic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-MediumItalic.ttf \
-    vendor/awaken/fonts/GoogleSans-Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Italic.ttf \
-    vendor/awaken/fonts/GoogleSans-Bold.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Bold.ttf \
-    vendor/awaken/fonts/GoogleSans-BoldItalic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-BoldItalic.ttf \
-    vendor/awaken/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
+    $(LOCAL_PATH)/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
