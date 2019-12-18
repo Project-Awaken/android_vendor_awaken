4cc0f7808419bca6f581ed761de052abd0f83f47
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-12-18 18:36:05 +0000

    vendor: Allow providing camera server and service
    Change-Id: I6d56930f4673cf7bdf99fccb87c15f0c0fc9a1d7
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- config/BoardConfigAwaken.mk
+++ config/BoardConfigAwaken.mk
@@ -1,2 +1,8 @@
 include vendor/awaken/config/BoardConfigKernel.mk
 include vendor/awaken/config/BoardConfigSoong.mk
+
+ifneq ($(TARGET_USES_PREBUILT_CAMERA_SERVICE), true)
+PRODUCT_SOONG_NAMESPACES += \
+    frameworks/av/camera/cameraserver \
+    frameworks/av/services/camera/libcameraservice
+endif
