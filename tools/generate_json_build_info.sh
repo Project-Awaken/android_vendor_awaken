a29bf144b4354cb0aebcf75b5ec6c43750544eec
Ido Ben-Hur <idoybh2@gmail.com>
2020-01-03 18:37:56 +0200

    vendor: tools: Make json generator add new lines
    Change-Id: I15f2e42eab4a65c711d99f119a46b6add0210ae4
    
    -----
    
    vendor: tools: Rewrite json generator script
    
    * Make the script readable
    * Use our relevant values
    * Generate a link for SF
    * Add outputs with colors
    
    Change-Id: Ib3f8839591aa12643b2ed145d95ac5a8ddd3fd14
    
    -----
    
    vendor: tools: Allow forcing json generation
    
    and only print required values for OpenDelta
    * force the json generation with 'export FORCE_JSON=1' before compiling
    
    -----
    
    vendor: tools: Rename json to the product name
    
    Change-Id: I9fdc26b0b93d84efb7c9b41d64886b915fb35291

--- tools/generate_json_build_info.sh
+++ tools/generate_json_build_info.sh
@@ -1,13 +1,31 @@
-#!/bin/sh
-if [ "$1" ]
-then
-  file_path=$1
-  file_name=$(basename "$file_path")
-  if [ -f $file_path ]; then
-    file_size=$(stat -c%s $file_path)
-    md5=$(cat "$file_path.md5sum" | cut -d' ' -f1)
-    datetime=$(grep ro\.build\.date\.utc $OUT/system/build.prop | cut -d= -f2);
-    id=$(sha256sum $file_path | awk '{ print $1 }');
-    echo "{\n   \"filename\": \"$file_name\",\n   \"datetime\": $datetime,\n   \"size\": $file_size,\n   \"filehash\": \"$md5\",\n   \"id\": \"$id\"\n}" > $file_path.json
-  fi
+#!/bin/bash
+GREEN="\033[1;32m"
+YELLOW="\033[1;33m"
+NC="\033[0m"
+if [ "$1" ]; then
+    echo "Generating .json"
+    file_path=$1
+    file_name=$(basename "$file_path")
+    if [ -f $file_path ]; then
+        # only generate for official builds. unless forced with 'export FORCE_JSON=1'
+        if [[ $file_name == *"official"* ]] || [[ $FORCE_JSON == 1 ]]; then
+            if [[ $FORCE_JSON == 1 ]]; then
+                echo -e "${GREEN}Forced generation of json${NC}"
+            fi
+            datetime=$(date +%s)
+            echo "{" > $file_path.json
+            echo "  \"response\": [" >> $file_path.json
+            echo "    {" >> $file_path.json
+            echo "      \"datetime\": ${datetime}," >> $file_path.json
+            echo "      \"filename\": \"${file_name}\"" >> $file_path.json
+            echo "    }" >> $file_path.json
+            echo "  ]" >> $file_path.json
+            echo "}" >> $file_path.json
+            device_code=$(echo $file_name | cut -d'-' -f4)
+            mv "${file_path}.json" "./${device_code}.json"
+            echo -e "${GREEN}Done generating ${YELLOW}${device_code}.json${NC}"
+        else
+            echo -e "${YELLOW}Skipped generating json for a non-official build${NC}"
+        fi
+    fi
 fi
