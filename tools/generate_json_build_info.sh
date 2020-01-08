76c661ad6d273f42a86ee750dc510dd9c4d62360
Ido Ben-Hur <idoybh2@gmail.com>
2020-01-08 15:08:02 +0200

    vendor: tools: Rename json to the product name

--- tools/generate_json_build_info.sh
+++ tools/generate_json_build_info.sh
@@ -8,23 +8,22 @@ if [ "$1" ]; then
     file_name=$(basename "$file_path")
     if [ -f $file_path ]; then
         # only generate for official builds. unless forced with 'export FORCE_JSON=1'
-        if [[ $file_name == *"Official"* ]] || [[ $FORCE_JSON == 1 ]]; then
+        if [[ $file_name == *"OFFICIAL"* ]] || [[ $FORCE_JSON == 1 ]]; then
             if [[ $FORCE_JSON == 1 ]]; then
                 echo -e "${GREEN}Forced generation of json${NC}"
             fi
-            datetime=$(grep ro\.build\.date\.utc $OUT/system/build.prop | cut -d= -f2)
-            # id=$(sha256sum $file_path | awk '{ print $1 }')
-            # link="https://sourceforge.net/projects/project-awaken/files/${AWAKEN_BUILD}/${file_name}/download"
+            datetime=$(date +%s)
             echo "{" > $file_path.json
-            echo "   \"datetime\": ${datetime}," >> $file_path.json
-            echo "   \"filename\": \"${file_name}\"," >> $file_path.json
-            # echo "   \"id\": \"${id}\"," >> $file_path.json
-            # echo "   \"romtype\": \"Official\"," >> $file_path.json
-            # echo "   \"size\": ${file_size}," >> $file_path.json
-            # echo "   \"url\": \"${link}\"," >> $file_path.json
-            # echo "   \"version\": \"10\"" >> $file_path.json
+            echo "  \"response\": [" >> $file_path.json
+            echo "    {" >> $file_path.json
+            echo "      \"datetime\": ${datetime}," >> $file_path.json
+            echo "      \"filename\": \"${file_name}\"" >> $file_path.json
+            echo "    }" >> $file_path.json
+            echo "  ]" >> $file_path.json
             echo "}" >> $file_path.json
-            echo -e "${GREEN}Done generating ${YELLOW}${file_name}.json${NC}"
+            device_code=$(echo $file_name | cut -d'-' -f4)
+            mv "${file_path}.json" "./${device_code}.json"
+            echo -e "${GREEN}Done generating ${YELLOW}${device_code}.json${NC}"
         else
             echo -e "${YELLOW}Skipped generating json for a non-official build${NC}"
         fi
