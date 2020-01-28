8e21f4036418231a81ec8cce0eb131aa3a6d45c1
Jake Weinstein <jake@aospa.co>
2020-01-28 06:00:07 +0100

    configs: Skip boot jars check
     * Throws an error during compilation
       because of Qualcomm Performance JARs.
    
    Change-Id: I94f376d14134c80c14188b07eebca01933b7e47a

--- config/common.mk
+++ config/common.mk
@@ -125,6 +125,9 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 # Require all requested packages to exist
 $(call enforce-product-packages-exist-internal,$(wildcard device/*/$(LINEAGE_BUILD)/$(TARGET_PRODUCT).mk),product_manifest.xml rild Calendar Launcher3 Launcher3Go Launcher3QuickStep Launcher3QuickStepGo android.hidl.memory@1.0-impl.vendor vndk_apex_snapshot_package)
 
+# Skip boot JAR checks.
+SKIP_BOOT_JARS_CHECK := true
+
 # Bootanimation
 $(call inherit-product, vendor/awaken/config/bootanimation.mk)
 
