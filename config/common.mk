7cdc23e2b3aac3512c65173fa481f76bdd2c642c
Saikiran <bjsaikiran@gmail.com>
2020-09-19 16:12:01 +0200

    vendor: Inherit some pixel bits for gapps builds
    Signed-off-by: Saikiran <bjsaikiran@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -2,6 +2,7 @@ include vendor/awaken/build/core/vendor/*.mk
 
 ifeq ($(USE_GAPPS),true)
 $(call inherit-product, vendor/google/gms/config.mk)
+$(call inherit-product, vendor/google/pixel/config.mk)
 endif
 
 ifeq ($(PRODUCT_USES_QCOM_HARDWARE), true)
