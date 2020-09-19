c54232a1f175b2b41b1a601e3e9d3dcdcd56907f
Pig <pig.priv@gmail.com>
2020-09-19 08:44:33 +0800

    config: Setup soong namespaces for TARGET_USE_QTI_BT_STACK
    To opt-in for QTI BT addons, enable TARGET_USE_QTI_BT_STACK in BoardConfig.mk
    
    Reference: [https://github.com/LineageOS/android_vendor_qcom_opensource_bluetooth-commonsys-intf/blob/lineage-18.0/bt-system-opensource-product.mk]
    
    Change-Id: I6bf3e1dda6fe5dc66f6fafdb32a1daecb9616c84
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- build/core/ProductConfigQcom.mk
+++ build/core/ProductConfigQcom.mk
@@ -91,6 +91,12 @@ endif
 
 PRODUCT_SOONG_NAMESPACES += hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)
 
+ifeq ($(TARGET_USE_QTI_BT_STACK),true)
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/qcom/opensource/commonsys/packages/apps/Bluetooth \
+    vendor/qcom/opensource/commonsys/system/bt/conf
+endif #TARGET_USE_QTI_BT_STACK
+
 # Add data-ipa-cfg-mgr to PRODUCT_SOONG_NAMESPACES if needed
 ifneq ($(USE_DEVICE_SPECIFIC_DATA_IPA_CFG_MGR),true)
     PRODUCT_SOONG_NAMESPACES += vendor/qcom/opensource/data-ipa-cfg-mgr
--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -27,3 +27,7 @@ define addVar
 endef
 
 $(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
+
+ifneq ($(TARGET_USE_QTI_BT_STACK),true)
+PRODUCT_SOONG_NAMESPACES += packages/apps/Bluetooth
+endif #TARGET_USE_QTI_BT_STACK
