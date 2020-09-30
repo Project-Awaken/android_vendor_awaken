ec356541f484dd4cde48716681f4b5fb05ad8475
saikiran2001 <bjsaikiran@gmail.com>
2020-09-30 12:43:06 +0200

    branding: Refractor our versioning
    - Also bump the version to 1.2

--- build/core/props.mk
+++ build/core/props.mk
@@ -6,7 +6,9 @@ endif
 
 ADDITIONAL_BUILD_PROPERTIES += \
     ro.awaken.version=$(AWAKEN_BASE_VERSION)-$(AWAKEN_BUILD_TYPE)-$(BUILD_DATE)-$(BUILD_TIME) \
-    ro.awaken.base.version=$(AWAKEN_BASE_VERSION)-$(AWAKEN_BUILD_TYPE) \
+    ro.awaken.base.version=$(AWAKEN_BASE_VERSION) \
+    ro.awaken.codename=$(AWAKEN_CODENAME) \
+    ro.awaken.build.type=$(AWAKEN_BUILD_TYPE) \
     ro.mod.version=$(BUILD_ID)-$(BUILD_DATE)-$(AWAKEN_BASE_VERSION) \
     ro.awaken.fingerprint=$(ROM_FINGERPRINT) \
     ro.awaken.buildtype=$(AWAKEN_BUILD_TYPE) \
