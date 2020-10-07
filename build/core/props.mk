4dc05391dc979cdfe32fca5931320ba47a154c65
saikiran2001 <bjsaikiran@gmail.com>
2020-10-07 08:30:13 +0200

    vendor: Add platform version property
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- build/core/props.mk
+++ build/core/props.mk
@@ -7,6 +7,7 @@ endif
 ADDITIONAL_BUILD_PROPERTIES += \
     ro.awaken.version=$(AWAKEN_BASE_VERSION)-$(AWAKEN_BUILD_TYPE)-$(BUILD_DATE)-$(BUILD_TIME) \
     ro.awaken.base.version=$(AWAKEN_BASE_VERSION) \
+    ro.awaken.build.version=$(AWAKEN_PLATFORM_VERSION) \
     ro.awaken.codename=$(AWAKEN_CODENAME) \
     ro.awaken.build.type=$(AWAKEN_BUILD_TYPE) \
     ro.mod.version=$(BUILD_ID)-$(BUILD_DATE)-$(AWAKEN_BASE_VERSION) \
