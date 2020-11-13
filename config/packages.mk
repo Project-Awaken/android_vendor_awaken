b23f2789dec344fb8012ffac121c4af27e8276bb
saikiran2001 <bjsaikiran@gmail.com>
2020-11-13 13:31:01 +0100

    vendor: Build some required tools by default
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- config/packages.mk
+++ config/packages.mk
@@ -2,6 +2,55 @@
 PRODUCT_PACKAGES += \
     librsjni
 
+# Extra tools
+PRODUCT_PACKAGES += \
+    7z \
+    bash \
+    bzip2 \
+    curl \
+    getcap \
+    e2fsck \
+    fsck.ntfs \
+    gdbserver \
+    htop \
+    lib7z \
+    libsepol \
+    micro_bench \
+    mke2fs \
+    mkfs.ntfs \
+    mount.ntfs \
+    nano \
+    openvpn \
+    oprofiled \
+    pigz \
+    powertop \
+    setcap \
+    sqlite3 \
+    strace \
+    tune2fs \
+    unrar \
+    unzip \
+    vim \
+    wget \
+    zip
+
+# Exchange support
+PRODUCT_PACKAGES += \
+    Exchange2
+
+# Filesystems tools
+PRODUCT_PACKAGES += \
+    fsck.exfat \
+    fsck.ntfs \
+    mke2fs \
+    mkfs.exfat \
+    mkfs.ntfs \
+    mount.ntfs
+
+# rsync
+PRODUCT_PACKAGES += \
+    rsync
+
 # Awaken Packages
 PRODUCT_PACKAGES += \
     CustomDoze \
