TheParasiteProject/vendor_parasite_common

vendor: Import overlays from RD1A.201105.003.C1
Change-Id: I1e698ee67e168dd56284e043cabb0544cb5f0b45

config/common.mk
@@ -104,8 +104,14 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     media.recorder.show_manufacturer_and_model=true
 
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/aosp/overlay
-DEVICE_PACKAGE_OVERLAYS += vendor/aosp/overlay/common
+# Overlays
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    vendor/aosp/overlay \
+    vendor/aosp/overlay-pixel \
+
+DEVICE_PACKAGE_OVERLAYS += \
+    vendor/aosp/overlay/common \
+    vendor/aosp/overlay-pixel/common
 
 # TouchGestures
 PRODUCT_PACKAGES += \

Project-Awaken/android_vendor_awaken

Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

config/common.mk
@@ -103,5 +103,10 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 include vendor/awaken/config/fonts.mk
 
 # Overlays
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/awaken/overlay
-DEVICE_PACKAGE_OVERLAYS += vendor/awaken/overlay/common
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    vendor/awaken/overlay \
+    vendor/awaken/overlay-pixel \
+
+DEVICE_PACKAGE_OVERLAYS += \
+    vendor/awaken/overlay/common \
+    vendor/awaken/overlay-pixel/common