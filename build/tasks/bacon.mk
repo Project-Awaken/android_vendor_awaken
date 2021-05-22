02100205e734df10abde7f18670b33b4b70d6d7f
OdSazib <odsazib@gmail.com>
2021-05-22 09:53:36 +0600

    vendor: tools: Refactor json script for awaken OTA
    * Also print output according to awaken OTA
    
    Change-Id: I533cb75a18bd5cfd3d84b112ba5a30c243e0ea98

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -33,6 +33,6 @@ bacon: $(INTERNAL_OTA_PACKAGE_TARGET)
 	echo -e ${CL_BLD}${CL_GRN}"Zip: "${CL_RED} $(AWAKEN_TARGET_PACKAGE)${CL_RST}
 	echo -e ${CL_BLD}${CL_GRN}"SHA256: "${CL_RED}" `cat $(AWAKEN_TARGET_PACKAGE).sha256sum | awk '{print $$1}' `"${CL_RST}
 	echo -e ${CL_BLD}${CL_GRN}"Size:"${CL_RED}" `du -sh $(AWAKEN_TARGET_PACKAGE) | awk '{print $$1}' `"${CL_RST}
-	echo -e ${CL_BLD}${CL_GRN}"TimeStamp:"${CL_RED}" `cat $(PRODUCT_OUT)/system/build.prop | grep ro.build.date.utc | cut -d'=' -f2 | awk '{print $$1}' `"${CL_RST}
-	echo -e ${CL_BLD}${CL_GRN}"Integer Value:"${CL_RED}" `wc -c $(AWAKEN_TARGET_PACKAGE) | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_GRN}"ID:"${CL_RED}" `md5sum $(AWAKEN_TARGET_PACKAGE) | cut -d ' ' -f 1`"${CL_RST}
+	echo -e ${CL_BLD}${CL_GRN}"Path:"${CL_RED}" $(AWAKEN_TARGET_PACKAGE)"${CL_RST}
 	echo -e ${CL_BLD}${CL_RED}"================================================================================"${CL_RED}
