97158fcf3cd894575da94ad847b27f569fd52060
OdSazib <odsazib@gmail.com>
2021-05-28 17:35:22 +0600

    vendor: tools: Print output according to awaken OTA

--- tools/generate_json_build_info.sh
+++ tools/generate_json_build_info.sh
@@ -19,20 +19,20 @@ if [ "$1" ]; then
             fi
             file_size=$(stat -c%s $file_path)
             md5=$(cat "$file_path.md5sum" | cut -d' ' -f1)
-            datetime=$(grep -m1 ro\.build\.date\.utc ./out/target/product/$DEVICE/system/build.prop | cut -d= -f2);
+            datetime=$(grep ro\.build\.date\.utc ./out/target/product/$DEVICE/system/build.prop | cut -d= -f2);
             id=$(sha256sum $file_path | awk '{ print $1 }');
-            build_type=$(grep -m1 ro\.awaken\.build\.type ./out/target/product/$DEVICE/system/build.prop | cut -d= -f2);
-            base_version=$(grep -m1 ro\.awaken\.base\.version ./out/target/product/$DEVICE/system/build.prop | cut -d= -f2);
+            build_type=$(grep ro\.awaken\.build\.type ./out/target/product/$DEVICE/system/build.prop | cut -d= -f2);
+            base_version=$(grep ro\.awaken\.base\.version ./out/target/product/$DEVICE/system/build.prop | cut -d= -f2);
             link="https://sourceforge.net/projects/project-awaken/files/${DEVICE}/${file_name}/download"
             echo "{" > $file_path.json
             echo "  \"response\": [" >> $file_path.json
             echo "    {" >> $file_path.json
             echo "      \"datetime\": ${datetime}," >> $file_path.json
             echo "      \"size\": ${file_size}," >> $file_path.json
-            echo "      \"filehash\": "${md5}"," >> $file_path.json
+            echo "      \"filehash\": \"${md5}\"," >> $file_path.json
             echo "      \"filename\": \"${file_name}\"," >> $file_path.json
-            echo "      \"id\": "${id}"," >> $file_path.json
-            echo "      \"romtype\": "${build_type}"," >> $file_path.json
+            echo "      \"id\": \"${id}\"," >> $file_path.json
+            echo "      \"romtype\": \"${build_type}\"," >> $file_path.json
             echo "      \"version\": \"${base_version}\"," >> $file_path.json
             echo "      \"url\": \"${link}\"" >> $file_path.json
             echo "    }" >> $file_path.json
