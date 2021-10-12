05fdd905d169a11d036aca7f642ecdc7d2e282d6
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-10-12 16:59:55 +0530

    config: Exclude AOSP audio on gapps builds
    Change-Id: Ibc507b084f734154802a2473b8268cc8cf8398f9

--- config/common.mk
+++ config/common.mk
@@ -104,10 +104,12 @@ PRODUCT_COPY_FILES += \
     vendor/awaken/prebuilt/common/etc/sysconfig/lily_experience.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/lily_experience.xml
 
 # Include AOSP audio files
+ifneq ($(USE_GAPPS),true)
 include vendor/awaken/config/aosp_audio.mk
 
 # Include Awaken audio files
 include vendor/awaken/config/awaken_audio.mk
+endif
 
 # Do not include art debug targets
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
