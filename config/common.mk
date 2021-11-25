fbd5626dd9eb66fd21d32d432c5197ad5c0b1852
jhonboy121 <alfredmathew05@gmail.com>
2021-11-25 17:29:01 +0530

    vendor: allow hiding navbar [2/3]
    * overlay extracted from https://github.com/Magisk-Modules-Repo/HideNavBar. So credits goes to the developer of this module
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Change-Id: I241f486a56297e127ee730a1510ea0c22e4bd448

--- config/common.mk
+++ config/common.mk
@@ -167,6 +167,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     SystemUI
 
+# Inherit from rro_overlays config
+$(call inherit-product, vendor/awaken/config/rro_overlays.mk)
+
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/awaken/overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/awaken/overlay/common
 
