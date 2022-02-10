83212982e2b7b6d0f9b03e57df3f5ff7b5891872
jhenrique09 <jhsv09@gmail.com>
2022-02-10 11:25:28 -0300

    vendor: Bring back FaceUnlock
    Change-Id: I0beb2311a7fa469182b71c54f0773aac36b53354

--- config/common.mk
+++ config/common.mk
@@ -91,6 +91,17 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.control_privapp_permissions=enforce
 
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED ?= true
+ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
+PRODUCT_PACKAGES += \
+    FaceUnlockService
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.face_unlock_service.enabled=$(TARGET_FACE_UNLOCK_SUPPORTED)
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+endif
+
 # Fonts
 include vendor/awaken/config/fonts.mk
 
