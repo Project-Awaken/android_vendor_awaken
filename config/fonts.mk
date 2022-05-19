2c8966952a87c084e13c82d709847f8f97ab0402
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-05-19 15:57:22 +0530

    overlay: Add HarmonyOS Sans font
    Co-authored-by: Danny Lin <danny@kdrag0n.dev>
    Change-Id: Ib5b2797065294b92620a7840ba4b023429956ad7
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- config/fonts.mk
+++ config/fonts.mk
@@ -6,6 +6,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
 
 PRODUCT_PACKAGES += \
+    FontHarmonySansOverlay \
     FontInterOverlay \
     FontLinotteSourceOverlay \
     FontManropeOverlay \
