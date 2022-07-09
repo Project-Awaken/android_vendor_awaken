49e80328068d219b03022b79fe56618dfb1eecdb
PainKiller3 <ninadpatil100@gmail.com>
2022-07-09 18:11:45 +0200

    branding: Bump version to 2.9
    * Update default wallpaper
    
    Change-Id: I621cf7a9231e7923ef0d0c1f9a0692a30e6b962d

--- config/branding.mk
+++ config/branding.mk
@@ -15,8 +15,8 @@
 # limitations under the License.
 
 # Versioning System
-AWAKEN_CODENAME := Plasma
-AWAKEN_NUM_VER := 2.8
+AWAKEN_CODENAME := Aero
+AWAKEN_NUM_VER := 2.9
 
 TARGET_PRODUCT_SHORT := $(subst awaken_,,$(AWAKEN_BUILD_TYPE))
 
