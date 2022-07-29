382dbeaea223580b1de55f4443e0e23282f6e081
Vaisakh Murali <mvaisakh@statixos.com>
2022-07-29 03:35:34 +0000

    vendor: fonts: Add quicksand font
    Copyright of Andrew Paglinawan
    https://github.com/andrew-paglinawan/QuicksandFamily
    
    Change-Id: Iced0fe50d36e770ee12131fd22c89aab50b08e8a
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>
    Reviewed-on: https://review.statixos.com/c/android_vendor_statix/+/8114
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- config/fonts.mk
+++ config/fonts.mk
@@ -12,4 +12,5 @@ PRODUCT_PACKAGES += \
     FontManropeOverlay \
     FontOnePlusSansOverlay \
     FontOneplusSlateSourceOverlay \
-    FontOppoSansOverlay
+    FontOppoSansOverlay \
+    FontQuicksandOverlay
