LineageOS/android_vendor_lineage

config: Exclude LatinIME dictionaries from RRO overlays
Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/4991
Change-Id: I7722ac86d9d3f02f393b2c120341db5a2b648ff5

config/common_full_phone.mk
@@ -7,6 +7,7 @@ PRODUCT_PACKAGES += \
 
 # Include Lineage LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/lineage/overlay/dictionaries
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/lineage/overlay/dictionaries
 
 # Enable support of one-handed mode
 PRODUCT_PRODUCT_PROPERTIES += \

Project-Awaken/android_vendor_awaken

config/common_full_phone.mk
@@ -7,5 +7,6 @@ PRODUCT_PACKAGES += \
 
 # Include Lineage LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/awaken/overlay/dictionaries
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/awaken/overlay/dictionaries
 
 $(call inherit-product, vendor/awaken/config/telephony.mk)