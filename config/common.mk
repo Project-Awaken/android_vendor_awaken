d9a8b5b5a77618d989334433d23abf80082beb02
someone5678 <nemui3353@gmail.com>
2022-11-25 14:29:55 +0900

    vendor: config: common: Build BtHelper
    Change-Id: I7996342036c93879873d0f27cf60aac92822a659

--- config/common.mk
+++ config/common.mk
@@ -110,6 +110,10 @@ $(call inherit-product, vendor/awaken/config/bootanimation.mk)
 PRODUCT_PACKAGES += \
     build-manifest
 
+# BtHelper
+PRODUCT_PACKAGES += \
+    BtHelper
+
 # Awaken packages
 PRODUCT_PACKAGES += \
     AwakenWallpaperStub
