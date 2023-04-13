0a6b401bc1ce27dd56393de05b3c038273a3334b
nift4 <nift4@protonmail.com>
2023-04-14 00:37:39 +0200

    vendor: Remove useless addon.d file
    * Avoid detection as custom ROM
    
    Change-Id: I1fbd63cc116d53c7c366662b632163f6bc079bbf

--- config/common.mk
+++ config/common.mk
@@ -33,11 +33,7 @@ endif
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/awaken/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
-    vendor/awaken/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
-    vendor/awaken/prebuilt/common/bin/50-awaken.sh:$(TARGET_COPY_OUT_SYSTEM)/addon.d/50-awaken.sh
-
-PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
-    system/addon.d/50-awaken.sh
+    vendor/awaken/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions
 
 ifneq ($(strip $(AB_OTA_PARTITIONS) $(AB_OTA_POSTINSTALL_CONFIG)),)
 PRODUCT_COPY_FILES += \
