e359f0847567628adadeff3104995292d10b35e2
Nolen Johnson <johnsonnolen@gmail.com>
2023-09-03 16:34:35 -0400

    vendor: Import and copy neofetch to system_ext
    * Needs bash which is on system_ext.
    
    Change-Id: I2f208141c0d79ea236fbffb905c6d6c1a0bdd2bd

--- config/common.mk
+++ config/common.mk
@@ -191,6 +191,10 @@ PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
     system/%/libfuse-lite.so \
     system/%/libntfs-3g.so
 
+## neofetch
+PRODUCT_COPY_FILES += \
+    vendor/awaken/tools/neofetch:$(TARGET_COPY_OUT_SYSTEM_EXT)/bin/neofetch
+
 # Openssh
 PRODUCT_PACKAGES += \
     scp \
