e0b84691dd565934a5eb479a7331bde18ca49858
DroidFreak32 <rushabshah32@gmail.com>
2024-02-12 11:28:40 +0530

    awaken: products: Bring back awaken_sdk_phone_arm64 emulator target
    Change-Id: I0578608bd78e1a03f90b17de4ee558646ce9ef2f

--- build/target/product/AndroidProducts.mk
+++ build/target/product/AndroidProducts.mk
@@ -27,6 +27,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/awaken_gsi_tv_x86_64.mk \
     $(LOCAL_DIR)/awaken_sdk_car_arm64.mk \
     $(LOCAL_DIR)/awaken_sdk_car_x86_64.mk \
+    $(LOCAL_DIR)/awaken_sdk_phone_arm64.mk \
     $(LOCAL_DIR)/awaken_sdk_phone_x86.mk \
     $(LOCAL_DIR)/awaken_sdk_phone_x86_64.mk \
     $(LOCAL_DIR)/awaken_sdk_tv_arm.mk \
@@ -45,6 +46,7 @@ COMMON_LUNCH_CHOICES := \
     awaken_gsi_tv_x86_64-userdebug \
     awaken_sdk_car_arm64-userdebug \
     awaken_sdk_car_x86_64-userdebug \
+    awaken_sdk_phone_arm64-userdebug \
     awaken_sdk_phone_x86-userdebug \
     awaken_sdk_phone_x86_64-userdebug \
     awaken_sdk_tv_arm-userdebug \
